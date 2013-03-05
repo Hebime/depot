9bc1bce6d290d27b4c0408b8e4d2d374677fdfb1
Robbie Caputo <r.caputo@mademedicorp.com>
2013-03-05 17:08:59 -0500

    added lame image for deleting

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,8 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
-  	
+  	@cart = current_cart
+
   	#increment the number of times a user visits the store
   	@store_count = increment_store_count
   end
