fbd299e497d9ec328eb0db822e1dd393116d91ff
Robbie Caputo <r.caputo@mademedicorp.com>
2013-03-08 17:06:33 -0500

    added more code for checkout feature

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ActiveRecord::Base
-  attr_accessible :address, :email, :name, :pay_type
+  PAYMENT_TYPES = [ "Check", "Credit Card", "Purchase order" ]
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end
